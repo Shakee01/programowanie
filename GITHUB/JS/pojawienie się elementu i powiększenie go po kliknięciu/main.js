ee439df524bfa563e2dfc77825a2f401eafabc36
Patryk Mazur <123391878+Shakee01@users.noreply.github.com>
2025-04-13 15:31:54 +0200

    Add files via upload

---
+++ GITHUB/JS/pojawienie się elementu i powiększenie go po kliknięciu/main.js
@@ -0,0 +1,29 @@
+const btn = document.querySelector("button");
+const liItems = document.querySelectorAll("li");
+let fontSize = 10;
+
+// PĘTLA
+// btn.addEventListener("click", () => {
+//  console.log("działa");
+//  fontSize++;
+//  for (let i = 0; i < liItems.length; i++) {
+//   // if (!liItems[i].style.display) {
+//   //  console.log("dodałem block");
+//   //  liItems[i].style.display = "block";
+//   // }
+//   liItems[i].style.display = "block";
+//   liItems[i].style.fontSize = `${fontSize}px`;
+//  }
+
+// })
+
+// FOREACH
+
+btn.addEventListener("click", () => {
+
+ liItems.forEach((liItem) => {
+  liItem.style.display = "block";
+  liItem.style.fontSize = fontSize + "px";
+ })
+ fontSize++;
+})
